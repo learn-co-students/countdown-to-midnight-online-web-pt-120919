9fdc62b2bb304f8737a42d4802570ab557c7b781
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-13 01:08:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+#countdown_output = 10
+def countdown(countdown_output)
+  while countdown_output > 0
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
+  end
+    
+   "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+
+def countdown_with_sleep(sleep)
+  sleep 5
 end
