9ab834621916cdfd794e092cf1b8893440fa0dcd
travis Lawson <travislawclimb@gmail.com>
2019-12-16 08:22:33 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(Integer)
+  #countdown to 0
+  #puts "#{integer} SECOND(S)!"
+  while integer > 0
+   puts "#{integer} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
