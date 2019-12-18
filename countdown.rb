664bbfeb16c983806c82b088dbea9e4216084707
Nguyen Ha <nynha0526@gmail.com>
2019-12-18 17:32:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,20 @@
 
-def countdown(5)
-  while countdown < 5
-  puts "#{countdown} SECONDS!"
-  countdown -= 1
+def countdown(seconds_to_midnigh)
+  while seconds_to_midnight > 0
+  puts "#{seconds_to_midnight} SECONDS!"
+  seconds_to_midnight -= 1
   end
   
   puts "HAPPY NEW YEAR!"
 
+end
+
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+  sleep(1)
+  seconds_to_midnight -= log10
+  end
+  puts "HAPPY NEW YEAR" 
+  
 end
